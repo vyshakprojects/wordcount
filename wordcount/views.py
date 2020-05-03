bcf1bd7a79b68483fc1005792f68e0115bd3d328
Vyshak <vyshak.m@coviam.com>
2020-05-03 17:54:55 +0530

    initial commit

---
+++ wordcount/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def home(request):
+    return render(request, 'home.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+    worddict = {}
+    sorted_words = {}
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
+    sorted_words = sorted(worddict.items(), key=operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html', {'fulltext':fulltext,'count':len(wordlist),'sorted_words':sorted_words})
+
+def about(request):
+    return render(request, 'about.html')
